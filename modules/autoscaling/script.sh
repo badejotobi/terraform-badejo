6d50aabcd7d0a475eccc319337188c3942d4d595
badejotobi <133620914+badejotobi@users.noreply.github.com>
2024-02-18 22:23:18 -0800

    Update script.sh

--- modules/autoscaling/script.sh
+++ modules/autoscaling/script.sh
@@ -9,11 +9,11 @@ sudo chown -R ec2-user:apache /var/www/html
 sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
 find /var/www -type f -exec sudo chmod 0664 {} \;
 sudo amazon-linux-extras install epel -y 
+sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
 sudo yum install -y https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm 
 sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023
 sudo yum install mysql-community-server -y
 sudo systemctl start mysqld
-sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
 
 
 
